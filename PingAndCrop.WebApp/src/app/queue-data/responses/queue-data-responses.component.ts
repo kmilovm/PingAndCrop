479c07901d63849b7bf1b9a0d243bd5b2d73238f
¨kmilovm¨ <kmilovm@hotmail.com>
2024-03-01 11:52:34 -0500

    adding pagination to mat-table

--- PingAndCrop.WebApp/src/app/queue-data/responses/queue-data-responses.component.ts
+++ PingAndCrop.WebApp/src/app/queue-data/responses/queue-data-responses.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { QueueDataService } from '../../services/queue-data.service';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatTableDataSource,  MatTableModule } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 import { PacResponse } from '../../models/pacResponse';
 
 @Component({
   selector: 'app-queue-data-responses',
   standalone: true,
-  imports: [MatExpansionModule, MatTableModule, CommonModule],
+  imports: [MatExpansionModule, MatTableModule, MatPaginator, CommonModule],
   templateUrl: './queue-data-responses.component.html',
   styleUrl: './queue-data-responses.component.css'
 })
@@ -18,17 +19,23 @@ export class QueueDataResponsesComponent implements OnInit {
   displayedColumns: string[] = ['MessageId', 'CroppedResponse'];
   panelOpenState = true;
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
   constructor(private dataService: QueueDataService) {}
 
   ngOnInit() {
     this.dataService.fetchDataResponses().subscribe({
       next: (response:Array<PacResponse>) => {
         this.dataSource = new MatTableDataSource<PacResponse>(response);
+        this.dataSource.paginator = this.paginator;
         console.log('Fetched dataResponses:', response, "Date:",Date.now());
       },
       error: (error) => {
         console.log(error);
       }
     });
+  }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 }
